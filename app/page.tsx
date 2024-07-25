a9d7f80f66336def3cd604be0d05eabb61d4dee2
jdboachie <jdboachie@gmail.com>
2024-07-25 15:48:28 +0000

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ function EmailIcon() {
 }
 
 
-export const staggerChildrenVariants = {
+const staggerChildrenVariants = {
   hidden: { opacity: 0, y: 20 },
   show: { opacity: 1, y: 0, transition: { duration: 0.6, ease: 'easeOut' } },
 }
