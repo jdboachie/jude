622be9480bc64cf262762ff3f34b726d317a1069
Jude Boachie <jdboachie@gmail.com>
2025-01-07 21:15:21 +0000

    begin work on footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,9 @@
+import styles from './footer.module.css'
+
+function Footer() {
+  return (
+    <footer className={styles.footer}><p>✌️❤️</p></footer>
+  )
+}
+
+export default Footer
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata, Viewport } from "next";
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import Footer from "./components/footer";
 
 export const metadata: Metadata = {
   title: "Jude Boachie",
@@ -26,6 +27,7 @@ export default function RootLayout({
         {children}
         <Analytics />
         <SpeedInsights />
+        <Footer />
       </body>
     </html>
   );
