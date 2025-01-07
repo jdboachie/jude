b89ca4a654620c82a46899aeb2f120f0b11e0e0b
Jude Boachie <jdboachie@gmail.com>
2025-01-07 14:48:35 +0000

    writing mvp done

--- src/app/components/posts.tsx
+++ src/app/components/posts.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
+import styles from './posts.module.css'
 import { formatDate, getBlogPosts } from '../writing/utils'
 
 export async function BlogPosts() {
   const allBlogs = getBlogPosts()
 
   return (
-    <div className='hover:text-neutral-400 transition-colors duration-300 dark:text-neutral-200 dark:hover:text-neutral-600 grid '>
+    <div className={styles.blogpostlist}>
       {allBlogs
         .sort((a: { metadata: { publishedAt: string | number | Date } }, b: { metadata: { publishedAt: string | number | Date } }) => {
           if (
@@ -18,17 +19,16 @@ export async function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col p-2 hover:text-neutral-900 dark:hover:text-neutral-100"
+            className={styles.blogpost}
             href={`/writing/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="truncate w-[120px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
-              </p>
-              <p className="tracking-tight">
-                {post.metadata.title}
-              </p>
-            </div>
+            <p className={styles.posttitle}>
+              {post.metadata.title}
+            </p>
+            <div className='hr' />
+            <p className={styles.postdate}>
+              {formatDate(post.metadata.publishedAt, false)}
+            </p>
           </Link>
         ))}
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
 import "./globals.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-
 
 export const metadata: Metadata = {
   title: "Jude Boachie",
   description: "Crafting systems and code for people",
 };
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+}
 
 export default function RootLayout({
   children,
