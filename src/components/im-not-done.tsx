34ab05a64aefc8129abc59cd2f4a4a657a367493
Jude Boachie <jdboachie@gmail.com>
2025-03-18 14:49:37 +0000

    tweak imnotdone

--- src/components/im-not-done.tsx
+++ src/components/im-not-done.tsx
@@ -2,7 +2,7 @@ import { Badge } from "@/components/ui/badge"
 
 const IMNotDone = () => {
   return (
-    <div className='my-2 font-mono text-xs'>
+    <div className='my-2 text-xs'>
       <Badge variant={'secondary'} className="lg:col-start-1 mr-2 border border-neutral-300 dark:border-neutral-600">Draft</Badge>
       May have typos and incomplete thoughts.
     </div>
