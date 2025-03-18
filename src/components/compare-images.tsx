b983bcbaa5b23cd9ca1b6601e1ed71bdc78e69fd
Jude Boachie <jdboachie@gmail.com>
2025-03-18 15:41:05 +0000

    tweaks,sigh, dissatisfied

--- src/components/compare-images.tsx
+++ src/components/compare-images.tsx
@@ -17,7 +17,7 @@ const CompareImages = (
   }
 ) => {
   return (
-    <div className='mt-6 flex gap-4 sm:col-span-3 z-10'>
+    <div className='mt-6 gap-4 flex lg:col-span-3 z-10 max-lg:overflow-scroll w-full'>
       {images.map((image, index) => (
         <Image
           key={index}
@@ -25,7 +25,7 @@ const CompareImages = (
           alt={image.alt}
           width={7000}
           height={7000}
-          className={`${darkModeImages && 'dark:hidden'} h-fit w-full border border-neutral-300 dark:border-neutral-600 bg-neutral-100 dark:bg-neutral-800 rounded-xl`}
+          className={`${darkModeImages && 'dark:hidden'} max-lg:w-4/5 h-fit w-full border border-neutral-300 dark:border-neutral-600 bg-neutral-100 dark:bg-neutral-800 rounded-xl`}
         />
       ))}
       {darkModeImages && darkModeImages.map((image, index) => (
@@ -35,7 +35,7 @@ const CompareImages = (
           alt={image.alt}
           width={7000}
           height={7000}
-          className={`${darkModeImages && 'hidden'} dark:block h-fit w-full border border-neutral-300 dark:border-neutral-600 bg-neutral-100 dark:bg-neutral-800 rounded-xl`}
+          className={`${darkModeImages && 'hidden'} max-lg:w-4/5 dark:block h-fit w-full border border-neutral-300 dark:border-neutral-600 bg-neutral-100 dark:bg-neutral-800 rounded-xl`}
         />
       ))}
     </div>
