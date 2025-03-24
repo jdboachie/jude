d73b55ac06d11563f45244c1a8eea0b9c8e678e1
Jude Boachie <jdboachie@gmail.com>
2025-03-24 23:17:38 +0000

    fix header glitch, implement copy button

--- src/components/copy-button.tsx
+++ src/components/copy-button.tsx
@@ -10,45 +10,44 @@ import {
 import { Button } from './ui/button';
 import { TooltipArrow, TooltipContent, TooltipTrigger } from './ui/tooltip';
 
-const CopyButton = ({url}: {url: string}) => {
-  const [copied, setCopied] = React.useState<boolean>(false);
+const CopyButton = ({ url }: { url: string }) => {
+  const [copied, setCopied] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
   function handleClick() {
     navigator.clipboard.writeText(url)
       .then(() => {
         setCopied(true);
+        setOpen(true);
         setTimeout(() => {
           setCopied(false);
+          setOpen(false);
         }, 2500);
       })
-      .catch((error) => {
-        console.error("Failed to copy text: ", error);
-      });
+      .catch(console.error);
   }
 
   return (
-    <Tooltip.Root>
+    <Tooltip.Root open={open} onOpenChange={setOpen}>
       <TooltipTrigger>
         <Button
-          size={'icon'}
-          variant={'secondary'}
+          size="icon"
+          variant="secondary"
           onClick={handleClick}
-          className={'rounded-full absolute top-1 right-0 border'}
+          className="rounded-full absolute top-1 right-0 border"
         >
-          <>
-            <span className="sr-only">{url}</span>
-            {copied ? <CheckIcon className='size-4' /> : <LinkIcon className='size-4' />}
-          </>
+          <span className="sr-only">{url}</span>
+          {copied ? <CheckIcon className="size-4 transition-transform scale-110" /> : <LinkIcon className="size-4" />}
         </Button>
       </TooltipTrigger>
       <Tooltip.Portal>
         <TooltipContent sideOffset={5}>
-          Copy link
+          {copied ? "Copied!" : "Copy link"}
           <TooltipArrow />
         </TooltipContent>
       </Tooltip.Portal>
     </Tooltip.Root>
-  )
-}
+  );
+};
 
-export default CopyButton
+export default CopyButton;
