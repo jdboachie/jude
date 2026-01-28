b1d0c15ffc21f1c1c4f815c292151580b39ef3de
Jude Boachie <jude.boachie@amalitech.com>
2026-01-28 13:23:56 +0000

    Add Socials component and update layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { SvelteDate } from 'svelte/reactivity';
+	import Socials from '$lib/components/Socials.svelte';
 
 	let date = new SvelteDate();
 
@@ -25,30 +26,86 @@
 </script>
 
 <main class="mt-22 antialiased">
-	<p class="mb-4 font-mono text-xs text-muted uppercase">{ghanaTime} - KUMASI, GHANA</p>
+	<div class="flex items-center justify-between pb-8">
+		<div class="font-mono text-xs text-muted uppercase">
+			{ghanaTime} •
+			<a
+				href="https://share.google/1qNdmRhFQVQNs0mCw"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="hover:text-black"
+			>
+				KUMASI, GHANA
+			</a>
+		</div>
+		<a class="font-mono text-xs text-muted hover:text-black" href="https://cal.com/judeboachie/15min">
+			SCHEDULE A MEETING
+		</a>
+	</div>
 	<p class="mb-12">
-		Jude Boachie. Software Engineer at
+		Jude Boachie.
 		<svg
-			class="inline-block size-6"
+			class="text-inline-icon"
 			xmlns="http://www.w3.org/2000/svg"
-			width="32"
-			height="32"
-			fill="currentColor"
-			viewBox="0 0 256 256"
-			><path
-				d="M216,56H176V48a24,24,0,0,0-24-24H104A24,24,0,0,0,80,48v8H40A16,16,0,0,0,24,72V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V72A16,16,0,0,0,216,56ZM96,48a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96ZM216,72v41.61A184,184,0,0,1,128,136a184.07,184.07,0,0,1-88-22.38V72Zm0,128H40V131.64A200.19,200.19,0,0,0,128,152a200.25,200.25,0,0,0,88-20.37V200ZM104,112a8,8,0,0,1,8-8h32a8,8,0,0,1,0,16H112A8,8,0,0,1,104,112Z"
-			></path></svg
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
 		>
+			<path d="M12 12h.01" />
+			<path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" />
+			<path d="M22 13a18.15 18.15 0 0 1-20 0" />
+			<rect width="20" height="14" x="2" y="6" rx="2" />
+		</svg>
+		Software Engineer at
 		<a
 			href="https://amalitech.com"
 			target="_blank"
 			rel="noopener noreferrer"
-			class="cursor-pointer transition-colors duration-300 ease-out hover:text-[#ff5a00]"
+			class="cursor-pointer transition-colors delay-300 duration-100 ease-out hover:text-[#ff5a00]"
 		>
 			Amalitech
 		</a>.
-		Fullstack web developer. Bachelor's in Computer Engineering. Light of the world.
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			class="text-inline-icon"
+		>
+			<path
+				d="M12.034 12.681a.498.498 0 0 1 .647-.647l9 3.5a.5.5 0 0 1-.033.943l-3.444 1.068a1 1 0 0 0-.66.66l-1.067 3.443a.5.5 0 0 1-.943.033z"
+			/>
+			<path d="M21 11V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h6" />
+		</svg>
+		Fullstack web developer.
+		<svg
+			class="text-inline-icon"
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			><path
+				d="M21.42 10.922a1 1 0 0 0-.019-1.838L12.83 5.18a2 2 0 0 0-1.66 0L2.6 9.08a1 1 0 0 0 0 1.832l8.57 3.908a2 2 0 0 0 1.66 0z"
+			/><path d="M22 10v6" /><path d="M6 12.5V16a6 3 0 0 0 12 0v-3.5" /></svg
+		>
+		Bachelor's in Computer Engineering. Light of my world.
 	</p>
+	<!-- <Socials /> -->
 	<span class="font-mono text-xs text-muted">ABOUT</span>
 	<p class="mb-12">
 		Versatile and detail-oriented software developer skilled in Python, Javascript/TypeScript, and
